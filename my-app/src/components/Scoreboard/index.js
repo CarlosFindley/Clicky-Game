dfc7e5a5b735f1c83857895c303acc332519d923
Carlos Villalta <CarlosFindley@protonmail.com>
2019-02-20 03:13:06 -0800

    update

--- my-app/src/components/Scoreboard/index.js
+++ my-app/src/components/Scoreboard/index.js
@@ -5,7 +5,7 @@ const Scoreboard = props => (
     <div className="score-container">
       <div className="row justify-content-center">
         <h2>Score: {props.score}</h2>
-        <h2>High Score: {props.topScore}</h2>
+        <h2>Highest Score: {props.topScore}</h2>
       </div>
       <div className="row justify-content-center">
       <h5 className="description">Click on the Game of Thrones character only once!</h5>
