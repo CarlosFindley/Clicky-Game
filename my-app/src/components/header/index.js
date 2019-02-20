8e1c46c322840bb707d41441fe218283f248846b
Carlos Villalta <CarlosFindley@protonmail.com>
2019-02-20 02:54:13 -0800

    clicky game of thrones game

---
+++ my-app/src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+    
+	return (
+		<header>
+            <div className="header-container">
+                <img src="./assets/thrones.png"  alt="nebula"/>
+            </div>
+		</header >
+	);
+}
+
+export default Header;
