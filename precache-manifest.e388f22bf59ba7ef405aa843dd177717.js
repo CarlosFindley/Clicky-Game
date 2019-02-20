bc1416680c76fd8fde1fbc5a56dc4af245f50b7d
Carlos Villalta <CarlosFindley@protonmail.com>
2019-02-20 03:07:06 -0800

    Updates

---
+++ precache-manifest.e388f22bf59ba7ef405aa843dd177717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "2e7d6fa4dca6078e5b9f",
+    "url": "/Clicky-Game/static/js/main.2e7d6fa4.chunk.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/Clicky-Game/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "2e7d6fa4dca6078e5b9f",
+    "url": "/Clicky-Game/static/css/main.cea51559.chunk.css"
+  },
+  {
+    "revision": "109752e71d339fae5a5e7ce210879489",
+    "url": "/Clicky-Game/index.html"
+  }
+];
